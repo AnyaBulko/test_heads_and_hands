5d97b132829a89be6728dfb393e009f493da36d5
Anya Bulko <anya_bulko@mail.ru>
2023-10-05 01:53:03 +0300

    Добавила логирование действий во время атаки

--- src/gameCharacters/Creature.java
+++ src/gameCharacters/Creature.java
@@ -8,6 +8,8 @@ public abstract class Creature {
     static final int MIN_NUMBER_FOR_SUCCESS = 5;
     public static final int MAX_ALLOWED_HEALTH = 100;
     public static final int MAX_ALLOWED_DAMAGE_PARAMETER = 30;
+    public static final int MAX_ALLOWED_ATTACK_PARAMETER = 30;
+    public static final int MAX_ALLOWED_DEFENSE_PARAMETER = 30;
     /*
      * У Существа есть параметры Атака и Защита. Это целые числа от 1 до 30.
      */
@@ -16,24 +18,22 @@ public abstract class Creature {
     /*
      * Натуральное число от 0 до N
      * Если Здоровье становится равным 0, то Существо умирает.
-     *
      * Игрок может себя исцелить до 4-х раз на 30% от максимального Здоровья.
      */
     private int maxHealth;
     private int currentHealth;
-
     /*
      * У Существа есть параметр Урон. Это диапазон натуральных чисел M - N. Например, 1-6.
      */
     private int minDamageParameter;
     private int maxDamageParameter;
-
     private boolean isDead = false;
+    private StringBuilder log;
 
     public Creature(int attackParameter, int defenseParameter, int maxHealth, int minDamageParameter, int maxDamageParameter) {
-        if (attackParameter > 30 || attackParameter < 1)
+        if (attackParameter > MAX_ALLOWED_ATTACK_PARAMETER || attackParameter < 1)
             throw new IllegalArgumentException("Параметр Атака должен быть в диапазоне от 1 до 30");
-        if (defenseParameter > 30 || defenseParameter < 1)
+        if (defenseParameter > MAX_ALLOWED_DEFENSE_PARAMETER || defenseParameter < 1)
             throw new IllegalArgumentException("Параметр Защита должен быть в диапазоне от 1 до 30");
         if (maxHealth > MAX_ALLOWED_HEALTH || maxHealth < 1)
             throw new IllegalArgumentException("Изначальное Здоровье существа должно быть в диапазоне от 1 до " +
@@ -48,6 +48,8 @@ public abstract class Creature {
         this.maxDamageParameter = maxDamageParameter;
 
         this.currentHealth = maxHealth;
+
+        log = new StringBuilder();
     }
 
     /*
@@ -74,35 +76,37 @@ public abstract class Creature {
     private boolean isAttackSuccess(int numberOfDiceRoll) {
         for (int i = 0; i < numberOfDiceRoll; i++) {
             int diceNumber = getRandomDiceNumber();
-            System.out.println("Бросок кубика " + (i + 1) + " = " + diceNumber);
+            log.append("Бросок кубика ").append(i + 1).append(" = ").append(diceNumber).append("\n");
             if (diceNumber >= MIN_NUMBER_FOR_SUCCESS)
                 return true;
         }
         return false;
     }
 
+    // Атака существа. Для каждой атаки ведется запись осуществленных действий,
+    // чтобы потом вывести пользователю.
     public void attack(Creature targetCreature) {
+        // Очищаем строку перед каждой атакой
+        log.setLength(0);
         int attackModifier = getAttackModifier(targetCreature);
-        System.out.println("Модификатор атаки = " + attackModifier);
+        log.append("Модификатор атаки = ").append(attackModifier).append("\n");
 
         int numberOfDiceRoll = attackModifier > 0 ? attackModifier : 1;
-        System.out.println("Количество бросков кубика = " + numberOfDiceRoll);
+        log.append("Количество бросков кубика = ").append(numberOfDiceRoll).append("\n");
 
         if (isAttackSuccess(numberOfDiceRoll)) {
-            System.out.println("Атака успешна!");
+            log.append("Атака успешна!\n");
             int damage = getRandomDamageParameter();
-            System.out.println("Нанесенный урон = " + damage);
+            log.append("Нанесенный урон = ").append(damage).append("\n");
+
             targetCreature.currentHealth -= damage;
-//            targetCreature.currentHealth -= getRandomDamageParameter();
-            if (targetCreature.currentHealth <= 0){
+
+            if (targetCreature.currentHealth <= 0) {
                 targetCreature.isDead = true;
                 targetCreature.currentHealth = 0;
             }
-
-
         } else
-            System.out.println("Атака неуспешная!");
-
+            log.append("Атака неуспешная!\n");
     }
 
     @Override
@@ -112,7 +116,8 @@ public abstract class Creature {
                 ", defenseParameter = " + defenseParameter +
                 ", maxHealth = " + maxHealth +
                 ", currentHealth = " + currentHealth +
-                ", DamageParameter = " + minDamageParameter + " - " + maxDamageParameter +
+                ", minDamageParameter = " + minDamageParameter +
+                ", minDamageParameter = " + maxDamageParameter +
                 ", isDead = " + isDead +
                 '}';
     }
@@ -129,5 +134,34 @@ public abstract class Creature {
 
     public int getMaxHealth() {
         return maxHealth;
+    }
+
+    public int getAttackParameter() {
+        return attackParameter;
+    }
+
+    public int getDefenseParameter() {
+        return defenseParameter;
+    }
+
+    public int getMinDamageParameter() {
+        return minDamageParameter;
+    }
+
+    public int getMaxDamageParameter() {
+        return maxDamageParameter;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public StringBuilder getLog() {
+        return log;
+    }
+
+    public void setNewLog(String newLog) {
+        log.setLength(0);
+        log.append(newLog);
     }
 }
