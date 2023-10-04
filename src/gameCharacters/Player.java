adb0e802484640a960701e6886165a6ce36d5b78
Anya Bulko <anya_bulko@mail.ru>
2023-10-05 01:56:42 +0300

    Добавила количество исцелений персонажу

--- src/gameCharacters/Player.java
+++ src/gameCharacters/Player.java
@@ -1,17 +1,35 @@
 package gameCharacters;
 
 public class Player extends Creature {
+
     public Player(int attackParameter, int defenseParameter, int maxHealth, int minDamageParameter, int maxDamageParameter) {
         super(attackParameter, defenseParameter, maxHealth, minDamageParameter, maxDamageParameter);
+        currentNumberOfHealing = 4;
     }
 
-    private void heal() {
-        int calculatedHealth = (int) (getCurrentHealth() + 0.3 * getMaxHealth());
-        setCurrentHealth(Math.min(calculatedHealth, getMaxHealth()));
+    public final static int NUMBER_OF_HEALING = 4;
+    private int currentNumberOfHealing;
+
+    public boolean heal() {
+        if (currentNumberOfHealing == 0) {
+            setNewLog("Исцеление невозможно. Количество исцелений = 0\n");
+            return false;
+        } else {
+            currentNumberOfHealing--;
+            int calculatedHealth = (int) (getCurrentHealth() + 0.3 * getMaxHealth());
+            int newHealth = Math.min(calculatedHealth, getMaxHealth());
+            setNewLog("Игрок исцелен на " + (newHealth - getCurrentHealth()) + "\n");
+            setCurrentHealth(newHealth);
+            return true;
+        }
     }
 
     @Override
     public String toString() {
         return "gameCharacters.Player" + super.toString();
+    }
+
+    public int getCurrentNumberOfHealing() {
+        return currentNumberOfHealing;
     }
 }
